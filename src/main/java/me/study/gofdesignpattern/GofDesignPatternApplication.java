ac6744eef740a48029bd51b709f4a3845fc4e48b
jinseonghwang <eddy5360@naver.com>
2022-11-25 21:44:43 +0900

    feat: init project

---
+++ src/main/java/me/study/gofdesignpattern/GofDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package me.study.gofdesignpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GofDesignPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GofDesignPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/study/gofdesignpattern/GofDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.gofdesignpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GofDesignPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
