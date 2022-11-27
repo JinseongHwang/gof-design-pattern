695f88d12fb219152d9d49dc959dcfa7436f65a6
jinseonghwang <eddy5360@naver.com>
2022-11-27 21:51:52 +0900

    feat: 직렬화&역직렬화 싱글턴 깨지는 문제 해결

--- src/main/java/me/study/gofdesignpattern/creational_patterns/singleton/Settings5.java
+++ src/main/java/me/study/gofdesignpattern/creational_patterns/singleton/Settings5.java
@@ -17,4 +17,10 @@ public class Settings5 implements Serializable {
     public static Settings5 getInstance() {
         return SettingsHolder.INSTANCE;
     }
+
+    // 역직렬화 과정에서 반드시 readResolve() 메서드를 찾아서 실행하게 되는데, 원래는 new XXX()가 실행된다.
+    // 하지만 여기서 getInstance() 를 호출하도록 하면 역직렬화 과정에서 싱글턴이 깨지는 문제를 해결할 수 있다.
+    protected Object readResolve() {
+        return getInstance();
+    }
 }
