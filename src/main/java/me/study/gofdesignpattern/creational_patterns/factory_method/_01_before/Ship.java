823a7359ec88cf054f022510e046f2ce6c9be9e3
jinseonghwang <eddy5360@naver.com>
2023-03-31 20:59:04 +0900

    feat: factory method before

---
+++ src/main/java/me/study/gofdesignpattern/creational_patterns/factory_method/_01_before/Ship.java
@@ -0,0 +1,15 @@
+package me.study.gofdesignpattern.creational_patterns.factory_method._01_before;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Ship {
+
+    private String name;
+    private String color;
+    private String logo;
+}
