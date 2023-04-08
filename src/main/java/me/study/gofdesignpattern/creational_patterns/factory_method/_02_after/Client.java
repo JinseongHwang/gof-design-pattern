52034891c5414855108411e469ac644316e69ce3
jinseonghwang <eddy5360@naver.com>
2023-04-09 02:28:56 +0900

    feat: factory method di

--- src/main/java/me/study/gofdesignpattern/creational_patterns/factory_method/_02_after/Client.java
+++ src/main/java/me/study/gofdesignpattern/creational_patterns/factory_method/_02_after/Client.java
@@ -3,13 +3,18 @@ package me.study.gofdesignpattern.creational_patterns.factory_method._02_after;
 // 새로운 구현체가 생길 때, 기존 코드는 변경되지 않아도 된다는 장점이 있다.
 // WhiteShip 만 사용하다가 BlackShip을 추가했을 경우에는 추가만 하면 된다.
 // OCP 원칙이 잘 지켜진 경우이다. -> 확장에는 열려있으며 변경에는 닫혀있다.
+//
+// 단, Client 측에서는 구체적인 클래스에 의존하기 때문에 코드 변경이 생길 수도 있다.
+// 이를 최소화하기 위해서는 인터페이스 기반의 DI를 사용하면 어느 정도 해결이 가능하다.
 public class Client {
 
     public static void main(String[] args) {
-        Ship whiteship = new WhiteShipFactory().orderShip("Whiteship", "jinseong@gmail.com");
-        System.out.println(whiteship);
+        final Client client = new Client();
+        client.print(new WhiteShipFactory(), "Whiteship", "jinseong@gmail.com");
+        client.print(new BlackShipFactory(), "Blackship", "jinseong@gmail.com");
+    }
 
-        Ship blackship = new BlackShipFactory().orderShip("Blackship", "jinseong@gmail.com");
-        System.out.println(blackship);
+    private void print(ShipFactory shipFactory, String name, String email) {
+        System.out.println(shipFactory.orderShip(name, email));
     }
 }
