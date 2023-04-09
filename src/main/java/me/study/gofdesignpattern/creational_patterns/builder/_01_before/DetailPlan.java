77ea7696ff5f0310079f438831aff2f9b5ee9adb
jinseonghwang <eddy5360@naver.com>
2023-04-09 18:13:43 +0900

    feat: builder pattern before

---
+++ src/main/java/me/study/gofdesignpattern/creational_patterns/builder/_01_before/DetailPlan.java
@@ -0,0 +1,19 @@
+package me.study.gofdesignpattern.creational_patterns.builder._01_before;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class DetailPlan {
+
+    private int day;
+    private String plan;
+
+    public DetailPlan(int day, String plan) {
+        this.day = day;
+        this.plan = plan;
+    }
+}
---
+++ src/main/java/me/study/gofdesignpattern/creational_patterns/builder/_01_before/TourPlan.java
@@ -0,0 +1,36 @@
+package me.study.gofdesignpattern.creational_patterns.builder._01_before;
+
+import java.time.LocalDate;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class TourPlan {
+
+    private String title;
+    private int nights;
+    private int days;
+    private LocalDate startDate;
+    private String whereToStay;
+    private List<DetailPlan> plans;
+
+    public TourPlan() {
+    }
+
+    public TourPlan(String title, int nights, int days, LocalDate startDate, String whereToStay, List<DetailPlan> plans) {
+        this.title = title;
+        this.nights = nights;
+        this.days = days;
+        this.startDate = startDate;
+        this.whereToStay = whereToStay;
+        this.plans = plans;
+    }
+
+    public void addPlan(int day, String plan) {
+        this.plans.add(new DetailPlan(day, plan));
+    }
+}
